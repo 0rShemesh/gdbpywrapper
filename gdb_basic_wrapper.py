6b5de0a24c67e86eba5d696073e6f674e8f21dbb
Or Shemesh <none@OrShemesh.localdomain>
2020-09-11 21:55:39 +0300

    added comments

--- gdb_basic_wrapper.py
+++ gdb_basic_wrapper.py
@@ -1,7 +1,6 @@
 import pexpect
 import gdb_constants
 
-
 class GdbClientBasicProccessWrapper:
     """
     this is the basic gdb executable wrapper
@@ -10,10 +9,19 @@ class GdbClientBasicProccessWrapper:
     def __init__(self, gdb_path, *args):
         self.process = pexpect.spawn(gdb_path, list(args))
 
-    def write(self, commmand_to_send):
+    def write(self, commmand_to_send: str) -> int:
+        """
+        send command
+        return the number of bytes written
+        """
         return self.process.sendline(commmand_to_send)
 
-    def read(self,timeout=5):
+    def read(self,timeout=5) -> str:
+        """
+        read data from the gdb
+        return the output of the cli
+        may raise pexpect.TIMEOUT
+        """
         self.process.expect(gdb_constants.GDB_START_LINE,timeout=timeout)
         return self.process.before
     
